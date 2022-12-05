02787b7605fc4be235994c28c30857cd16df54e1
aventinodev <108619445+aventinodev@users.noreply.github.com>
2022-12-05 11:14:55 +0000

    Deploying to gh-pages from @ aventinodev/goit-js-hw-11@925cdb6be4fbc574315b9b1f88fd3687ae307d99 🚀

---
+++ index.92482b4a.js
@@ -0,0 +1,2 @@
+const e={wrapp:document.querySelector(".wrapp-form"),body:document.querySelector("body")};let o=e.wrapp.getBoundingClientRect().height;console.log(o),e.body.style.paddingTop=`${o}px`;
+//# sourceMappingURL=index.92482b4a.js.map

9312d0be43ced8d1204ee1f432e355f6cc213fc5
aventinodev <108619445+aventinodev@users.noreply.github.com>
2022-12-07 17:15:16 +0000

    Deploying to gh-pages from @ aventinodev/goit-js-hw-11@15a905011de9108727a45612b9e43184c65160c1 🚀

--- index.553c0ed7.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={wrapp:document.querySelector(".wrapp-form"),body:document.querySelector("body")},e=o.wrapp.getBoundingClientRect().height;console.log(e),o.body.style.paddingTop="".concat(e,"px")}();
-//# sourceMappingURL=index.553c0ed7.js.map

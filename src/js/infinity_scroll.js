e38a3f2a9f3cf853d799aa7bf20efb684d47097f
Olena Bondar <olena.bondar1060@gmail.com>
2022-12-08 22:14:38 +0200

    Create infinity_scroll.js

---
+++ src/js/infinity_scroll.js
@@ -0,0 +1,19 @@
+// var debounce = require('lodash.debounce');
+
+// window.addEventListener('scroll', debounce(onInfinityScroll, 300));
+
+// function onInfinityScroll() {
+//   const query = document.querySelector('searchQuery');
+//   const documentRect = document.documentElement.getBoundingClientRect();
+
+//   console.log(documentRect.bottom);
+//   if (documentRect.bottom < document.documentElement.clientHeight + 150) {
+//     console.log('done');
+//     page += 1;
+
+//     onFetch(searchQuery, page, perPage).then(({ hits, totalHits }) => {
+//       markupCardGallery(hits);
+//     });
+//   }
+// }
+// .firstElementChild.getBoundingClientRect().height;

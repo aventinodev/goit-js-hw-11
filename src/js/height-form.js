0fbcbd0cc6798f5c6a93d0fb1c5a05d30162453b
Olena Bondar <olena.bondar1060@gmail.com>
2022-12-08 22:14:30 +0200

    Update height-form.js

--- src/js/height-form.js
+++ src/js/height-form.js
@@ -1,8 +1,8 @@
-const refs = {
-  wrapp: document.querySelector('.wrapp-form'),
-  body: document.querySelector('body'),
-};
+// const refs = {
+//   wrapp: document.querySelector('.wrapp-form'),
+//   body: document.querySelector('body'),
+// };
 
-let heightWrappForm = refs.wrapp.getBoundingClientRect().height;
-console.log(heightWrappForm);
-refs.body.style.paddingTop = `${heightWrappForm}px`;
+// let heightWrappForm = refs.wrapp.getBoundingClientRect().height;
+// console.log(heightWrappForm);
+// refs.body.style.paddingTop = `${heightWrappForm}px`;
